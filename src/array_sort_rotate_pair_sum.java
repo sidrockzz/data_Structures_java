2a7396209e5e1ea38cdee298afba35fb31999c0a
sidrockzz <marshtomp.1999@hotmail.com>
2021-03-31 13:51:11 +0530

    Given a sorted and rotated array, find if there is a pair with a given sum with count

--- src/array_sort_rotate_pair_sum.java
+++ src/array_sort_rotate_pair_sum.java
@@ -4,18 +4,26 @@ import java.util.Scanner;
 
 public class array_sort_rotate_pair_sum {
     //O(n)
-    static boolean pair(List<Integer> a,int n , int x){
+    static int pair(List<Integer> a,int n , int x){
         int i;
         for(i=0;i<n-1;i++)
             if(a.get(i)>a.get(i+1)) break;
         int l = (i+1)%n;
         int r =i;
+        int count =0;
         while (l!=r){
-            if(a.get(l)+a.get(r)==x) return true;
-            if(a.get(l)+a.get(r)<x) l = (l+1)%n;
+            if(a.get(l)+a.get(r)==x){
+                count++;
+                if(l==(r-1+n)%n){
+                    return count;
+                }
+                l = (l + 1) % n;
+                r = (r - 1 + n) % n;
+            }
+            else if(a.get(l)+a.get(r)<x) l = (l+1)%n;
             else r = (n+r-1)%n;
         }
-        return false;
+        return count;
     }
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
@@ -26,7 +34,6 @@ public class array_sort_rotate_pair_sum {
             int s = sc.nextInt();
             a.add(s);
         }
-        if(pair(a,n,sum)) System.out.println("Array has two elements");
-        else System.out.println("Doesnt have");
+        System.out.println(pair(a,n,sum));
     }
 }
