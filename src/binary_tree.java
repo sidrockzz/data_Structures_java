a7de4c1d071a70d9287296ade31b92dbffe2a1ae
sidrockzz <marshtomp.1999@hotmail.com>
2021-04-03 09:57:19 +0530

    Binary Tree

---
+++ src/binary_tree.java
@@ -0,0 +1,68 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class binary_tree {
+    static class Node{
+        int key;
+        Node left,right;
+        Node(int key){
+            this.key = key;
+            left=null;
+            right = null;
+        }
+    }
+    static Node root;
+    static Node temp = root;
+    static void inorder(Node temp){
+        if(temp == null) return;
+        inorder(temp.left);
+        System.out.print(temp.key+" ");
+        inorder(temp.right);
+    }
+    static void postorder(Node temp){
+        if(temp==null) return;
+        postorder(temp.left);
+        postorder(temp.right);
+        System.out.print(temp.key+" ");
+    }
+    static void preorder(Node temp){
+        if(temp==null) return;
+        System.out.print(temp.key+" ");
+        preorder(temp.left);
+        preorder(temp.right);
+    }
+    static void insert(Node temp,int key){
+        if(temp == null){
+            root = new Node(key);
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(temp);
+        while(!q.isEmpty()){
+            temp=q.peek();
+            q.remove();
+            if(temp.left==null){
+                temp.left = new Node(key);
+                break;
+            }
+            else q.add(temp.left);
+            if(temp.right==null){
+                temp.right = new Node(key);
+                break;
+            }
+            else q.add(temp.right);
+        }
+    }
+    public static void main(String [] args){
+        root = new Node(10);
+        insert(root,11);
+        insert(root,12);
+        insert(root,13);
+        insert(root,14);
+        inorder(root);
+        System.out.println();
+        preorder(root);
+        System.out.println();
+        postorder(root);
+    }
+}
